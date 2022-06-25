1f5b3d61452e9b5ffd3bee7130d7fa04aec3da19
Kanishk Khurana <kanishkkhurana152@gmail.com>
2022-06-25 06:46:06 +0530

    update half landing page

--- pages/landing/landing.js
+++ pages/landing/landing.js
@@ -1,174 +1,146 @@
 import React from "react";
 import Navbar from "../navbar";
 import Image from "next/image";
-import DoctorImage from "../../public/doctor-photo.jpg";
+import DoctorImage from "../../public/DoctorPhoto.png";
 
-// const ProcessTimeline = () => (
-//   <div>
-//     <ol class="items-center sm:flex">
-//       <li class="relative mb-6 sm:mb-0">
-//         <div class="flex items-center">
-//           <div class="flex z-10 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-//             <svg
-//               class="w-3 h-3 text-blue-600 dark:text-blue-300"
-//               fill="currentColor"
-//               viewBox="0 0 20 20"
-//               xmlns="http://www.w3.org/2000/svg"
-//             >
-//               <path
-//                 fill-rule="evenodd"
-//                 d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-//                 clip-rule="evenodd"
-//               ></path>
-//             </svg>
-//           </div>
-//           <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-//         </div>
-//         <div class="mt-3 sm:pr-8">
-//           <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-//             Flowbite Library v1.0.0
-//           </h3>
-//           <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-//             Released on December 2, 2021
-//           </time>
-//           <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-//             Get started with dozens of web components and interactive elements.
-//           </p>
-//         </div>
-//       </li>
-//       <li class="relative mb-6 sm:mb-0">
-//         <div class="flex items-center">
-//           <div class="flex z-10 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-//             <svg
-//               class="w-3 h-3 text-blue-600 dark:text-blue-300"
-//               fill="currentColor"
-//               viewBox="0 0 20 20"
-//               xmlns="http://www.w3.org/2000/svg"
-//             >
-//               <path
-//                 fill-rule="evenodd"
-//                 d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-//                 clip-rule="evenodd"
-//               ></path>
-//             </svg>
-//           </div>
-//           <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-//         </div>
-//         <div class="mt-3 sm:pr-8">
-//           <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-//             Flowbite Library v1.2.0
-//           </h3>
-//           <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-//             Released on December 23, 2021
-//           </time>
-//           <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-//             Get started with dozens of web components and interactive elements.
-//           </p>
-//         </div>
-//       </li>
-//       <li class="relative mb-6 sm:mb-0">
-//         <div class="flex items-center">
-//           <div class="flex z-10 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-//             <svg
-//               class="w-3 h-3 text-blue-600 dark:text-blue-300"
-//               fill="currentColor"
-//               viewBox="0 0 20 20"
-//               xmlns="http://www.w3.org/2000/svg"
-//             >
-//               <path
-//                 fill-rule="evenodd"
-//                 d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-//                 clip-rule="evenodd"
-//               ></path>
-//             </svg>
-//           </div>
-//           <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-//         </div>
-//         <div class="mt-3 sm:pr-8">
-//           <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-//             Flowbite Library v1.3.0
-//           </h3>
-//           <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-//             Released on January 5, 2022
-//           </time>
-//           <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-//             Get started with dozens of web components and interactive elements.
-//           </p>
-//         </div>
-//       </li>
-//     </ol>
-//   </div>
-// );
-
-const iFrontBanner = () => (
-  <div class="mb-4 mt-7 w-full">
-    <div class="flex justify-between flex-wrap  ">
-      <div class="mr-1 my-2 ml-2 w-[50%]">
-        <h1 class="font-sans text-5xl sm:text-3xl font-bold text-slate-800 sm:text-left select-none">
-          Find Your Doctor <br />
-          <span class="underline text-transparent text-7xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-            10X Faster!
-          </span>
-        </h1>
-        <div>
-          <p class="mt-3 break-words font-sans text-sm text-slate-500 select-none">
-            Locum helps you connect with Doctors-On-Demand. It is the quickest
-            interface between doctors and hospitals. Join us today and find a
-            patient that needs your help and heal the world.
-            Locum helps you connect with Doctors-On-Demand. It is the quickest
-            interface between doctors and hospitals. Join us today and find a
-            patient that needs your help and heal the world.
+const ProcessTimeline = () => (
+  <div class="p-12 bg-[#0E185F] h-full">
+    <h1 class="mb-4 font-sans text-3xl sm:text-4xl font-bold text-slate-50 text-center sm:text-left select-none"> How It Works? </h1>
+    <ol class="items-center sm:flex">
+      <li class="relative mb-6 sm:mb-0">
+        <div class="flex items-center">
+          <div class="flex z-10 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+            <svg
+              class="w-3 h-3 text-blue-600 dark:text-blue-300"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+          <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+        </div>
+        <div class="mt-3 border-2 rounded-lg p-2.5 mr-1.5 sm:pr-8 ">
+          <time class="block mb-1.5 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+          Step 1
+          </time>
+          <h3 class="text-lg font-semibold text-slate-50 dark:text-white mb-2">
+             Register on Locum
+          </h3>
+          <p class="text-base font-normal text-gray-400 dark:text-gray-400">
+            Fill the onboarding form and log in using your credentials.
           </p>
         </div>
-      </div>
-
-      <div class="mx-5 my-2 w-[20%] hidden sm:inline ">
-        <Image
-          src={DoctorImage}
-          class="mr-2 pr-2 h-6 sm:h-9 rounded-3xl justify-self-center border-8 border-double border-gray-50"
-          alt="Locum Logo"
-          width="200vw"
-          height="280vh"
-        />
-      </div>
-    </div>
+      </li>
+      <li class="relative mb-6 sm:mb-0">
+        <div class="flex items-center">
+          <div class="flex z-10 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+            <svg
+              class="w-3 h-3 text-blue-600 dark:text-blue-300"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+          <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+        </div>
+        <div class="mt-3 border-2 rounded-lg p-2.5 mr-1.5 sm:pr-8 ">
+          <time class="block mb-1.5 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            Step 2
+          </time>
+          <h3 class="text-lg font-semibold text-slate-50 dark:text-white mb-2">
+            Enter Details and Submit Documents 
+          </h3>
+          <p class="text-base font-normal text-gray-400 dark:text-gray-400">
+            Submit your bank details and educational qualifications.
+          </p>
+        </div>
+      </li>
+      <li class="relative mb-6 sm:mb-0">
+        <div class="flex items-center">
+          <div class="flex z-10 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+            <svg
+              class="w-3 h-3 text-blue-600 dark:text-blue-300"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+          <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+        </div>
+        <div class="mt-3 border-2 rounded-lg p-2.5 mr-1.5 sm:pr-8 ">
+          <time class="block mb-1.5 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            Step 3
+          </time>
+          <h3 class="text-lg font-semibold text-slate-50 dark:text-white mb-2">
+            Work with Hospitals
+          </h3>
+          <p class="text-base font-normal text-gray-400 dark:text-gray-400">
+            Choose from 100s of available on-demand jobs of your speciality.
+          </p>
+        </div>
+      </li>
+    </ol>
   </div>
 );
 
-const FrontBanner=()=>(
-  <div class="">
-    <div class="flex justify-between flex-wrap  ">
-      <div class="">
-        <h1 class="font-sans text-6xl sm:text-3xl font-bold text-slate-800 sm:text-left select-none">
+const FrontBanner = () => (
+  <div class="m-5 mb-8 h-auto">
+    <div class="flex m-4 justify-evenly flex-row flex-wrap  ">
+      <div class="m-4">
+        <h1 class="font-sans text-4xl sm:text-6xl font-bold text-slate-800 text-center sm:text-left select-none">
           Find Your Doctor <br />
-          <span class="underline text-transparent text-7xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+          <span class="underline text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
             10X Faster!
           </span>
         </h1>
         <div>
-          <p class="break-words font-sans text-sm text-slate-500 select-none">
+          <p class="mt-3 p-3 max-w-fit break-words font-sans text-sm text-slate-500 select-none">
             Locum helps you connect with Doctors-On-Demand. It is the quickest
             interface between doctors and hospitals. Join us today and find a
-            patient that needs your help and heal the world.
-            Locum helps you connect with Doctors-On-Demand. It is the quickest
+            patient that needs your help and heal the world.Locum helps you connect with Doctors-On-Demand. It is the quickest
             interface between doctors and hospitals. Join us today and find a
             patient that needs your help and heal the world.
           </p>
         </div>
+        <div class="mt-3 text-center sm:text-left">
+        <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 text-left sm:text-left">Register Now</button>
+        <button type="button" class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-8 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500/50 dark:focus:ring-blue-800">  Log In  </button>
+    </div>
       </div>
 
-      <div class=" hidden sm:inline ">
+      <div class="m-6 my-2 p-4 ml-8 ">
         <Image
           src={DoctorImage}
-          class="mr-0 pr-2 h-6 sm:h-9 rounded-3xl justify-self-center border-8 border-double border-gray-50"
+          class="mr-2 pr-2 h-6 sm:h-9 rounded-full justify-self-center shadow-xl shadow-sky-300"
           alt="Locum Logo"
-          width="200vw"
-          height="280vh"
+          width="260vw"
+          height="340vh"
         />
       </div>
     </div>
+
   </div>
-)
+);
+
+
 
 
 
@@ -176,8 +148,8 @@ export default function landing() {
   return (
     <div>
       <FrontBanner />
-
-      {/* <ProcessTimeline /> */}
+      <ProcessTimeline />
+ 
     </div>
   );
 }
